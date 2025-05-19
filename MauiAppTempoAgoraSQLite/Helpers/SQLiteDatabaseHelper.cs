c1f37a82bf48c5ffad01fb1b199a7d961c4d11a1
231024 <231024@CPD03-M18>
2025-05-19 10:52:41 -0300

    .

---
+++ MauiAppTempoAgoraSQLite/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiAppTempoAgoraSQLite.Models;
+using SQLite;
+
+namespace MauiAppTempoAgoraSQLite.Helpers
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        /// Construtor que inicializa a conexão e cria a tabela se não existir
+        public SQLiteDatabaseHelper(string path)
+        {
+            // Inicializa a conexão assíncrona com o banco de dados
+            _conn = new SQLiteAsyncConnection(path);
+            // Cria a tabela Produto se não existir
+            _conn.CreateTableAsync<Tempo>().Wait();
+        }
+
+        /// Insere um novo produto no banco de dados
+        public Task<int> Insert(Tempo temp)
+        {
+            return _conn.InsertAsync(temp);
+        }
+    }
+}
